73299cc28caee82306b99f085b0867602674db7d
brianf <brian@stability.ai>
2024-11-18 23:43:29 +0000

    wip

--- submit_eval.sh
+++ submit_eval.sh
@@ -15,6 +15,8 @@ DATASET=$2
 
 echo "Evaluating controlnet model: $CONTROLNET_CKPT with dataset: $DATASET"
 
+mkdir -p outputs/$CONTROLNET_CKPT
+
 python evaluate.py \
     --verbose True \
     --input_dataset $DATASET \
@@ -23,4 +25,5 @@ python evaluate.py \
     --controlnet_ckpt $CONTROLNET_CKPT \
     --vae "/weka/applied-shared/sd3_ref/sd3_vae.safetensors" \
     --sampler "euler" \
-    --text_encoder_device "cuda"
+    --text_encoder_device "cuda" \
+    --out_dir "outputs/$CONTROLNET_CKPT" \
